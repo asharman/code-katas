7a60aeeacd81d09828c79cb73e482db4acf43907
Austin Sharman <asharman22@gmail.com>
2020-08-09 23:39:28 -0400

    :sparkles: Complete RGB to Hex

--- 5_kyu/rgb_to_hex/index.js
+++ 5_kyu/rgb_to_hex/index.js
@@ -1,3 +1,11 @@
 function rgb(r, g, b){
-  // complete this function  
+  const colors = [r, g, b];
+  
+  return colors
+    .map(color => color > 255 ? 255 : color)
+    .map(color => color < 0 ? 0 : color)
+    .map(color => color.toString(16))
+    .map(color => color.length < 2 ? `0${color}` : color)
+    .join('')
+    .toUpperCase();
 }
